17ff8b9067a9ff69e659f8cfa2a8cbd243ac708d
mxmlndml <42516330+MXMLNDML@users.noreply.github.com>
2023-12-10 11:42:53 +0100

    add day 9

---
+++ 2023/day-09/src/bin/part-1.rs
@@ -0,0 +1,67 @@
+fn main() {
+    let input = include_str!("input.txt");
+    println!("{}", part1(input));
+}
+
+fn get_difference(history: &Vec<i32>) -> Vec<i32> {
+    let mut history = history.iter().peekable();
+    let mut difference: Vec<i32> = vec![];
+
+    loop {
+        let current = history.next().expect("no value");
+
+        if history.peek().is_none() {
+            break;
+        }
+
+        let next = history.peek().expect("no next value");
+
+        difference.push(*next - current);
+    }
+
+    difference
+}
+
+fn part1(input: &str) -> String {
+    input
+        .lines()
+        .fold(0, |acc, dataset| {
+            let history = dataset
+                .split(" ")
+                .map(|value| value.parse::<i32>().expect("value not numeric"))
+                .collect::<Vec<i32>>();
+
+            let mut sequences = vec![history.clone()];
+
+            loop {
+                let new_seq = get_difference(sequences.last().expect("no sequence found"));
+
+                if !new_seq.iter().any(|value| *value != 0) {
+                    break;
+                }
+
+                sequences.push(new_seq);
+            }
+
+            let mut forecast = 0;
+            sequences.iter().rev().for_each(|seq| {
+                forecast += seq.last().expect("no value in sequence");
+            });
+
+            acc + forecast
+        })
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_SOLUTION: &str = "114";
+
+    #[test]
+    fn example() {
+        let input = include_str!("example.txt");
+        assert_eq!(part1(input), EXAMPLE_SOLUTION.to_string());
+    }
+}
